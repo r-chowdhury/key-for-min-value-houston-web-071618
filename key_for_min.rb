bae1b191e6cb7cbab98ed10b5dd2af837e7efc54
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-06-20 15:27:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, num|
     if min_value == nil
       min_value = num
-      min_key = nam
+      min_key = name
     elsif min_value > num
       min_value = num
       min_key = name
